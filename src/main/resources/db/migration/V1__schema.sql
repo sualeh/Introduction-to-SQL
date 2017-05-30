4ed14c41071ae49f6aeed37146d2ab964ae32a42
Sualeh Fatehi <sualeh@hotmail.com>
2017-05-29 21:50:59 -0400

    Updated products to remove proprietary names; Added Purchases

--- src/main/resources/db/migration/V1__schema.sql
+++ src/main/resources/db/migration/V1__schema.sql
@@ -1,7 +1,13 @@
 CREATE TABLE Products (
-  Product VARCHAR(50) default NULL,
+  Product VARCHAR(50),
   Price DECIMAL,
-  Category VARCHAR(50) default NULL,
-  Manufacturer VARCHAR(50) default NULL
-)
+  Category VARCHAR(50),
+  Manufacturer VARCHAR(50)
+);
+
+CREATE TABLE Purchases (
+  Buyer VARCHAR(50),
+	Seller VARCHAR(50),
+	Product VARCHAR(50)
+);
 
--- src/main/resources/db/migration/V2__data.sql
+++ src/main/resources/db/migration/V2__data.sql
@@ -1,8 +1,19 @@
 INSERT INTO Products 
-(Product,Price,Category,Manufacturer) 
+(Product, Price, Category, Manufacturer) 
 VALUES 
-('Gizmo',19.99,'Gadgets','GizmoWorks'),
-('PowerGizmo',29.99,'Gadgets','GizmoWorks'),
-('SingleTouch',149.99,'Photography','Canon'),
-('MultiTouch',203.99,'Household','Hitachi')
+('Gizmo', 19.99, 'Gadgets', 'GizmoWorks'), 
+('PowerGizmo', 29.99, 'Gadgets', 'GizmoWorks'), 
+('Camera Plus', 149.99, 'Photography', 'Pear'), 
+('Clock', 203.99, 'Household', ' Clock Maker')
 ;
+
+INSERT INTO Purchases 
+(Buyer, Seller, Product) 
+VALUES 
+('Joe', 'Sam', 'Gizmo'), 
+('Alan', 'Fred', 'PowerGizmo'), 
+('Jill', 'Fred', 'Camera Plus'),
+('Alan', 'Fred', 'Camera Plus'),
+('Sue', 'Jane', 'Camera Plus')
+;
+
