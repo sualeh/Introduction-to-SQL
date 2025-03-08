855812a0607a9b4e3a86168f6e8a3f38ded154f4
Sualeh Fatehi <sualeh@hotmail.com>
2025-03-08 17:59:59 -0500

    Quries

--- create_database.py
+++ create_database.py
@@ -5,7 +5,7 @@ from sqlalchemy import text, Engine, Connection
 
 # Configure logger
 logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    level=logging.ERROR, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
